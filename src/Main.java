54a955a0fd6a5d29b4103d3a1c9211573809bab1
Vasily Demin <vasily.demin@gmail.com>
2022-04-20 20:28:31 +0300

    homework 9. Objects. Mandatory tasks (minor changes in the main method)

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,12 @@ public class Main {
                 "Петров", 2022);
         Author auth2 = book1.getAuthor();
         Book book2 = new Book("Малая энциклопедия Интернет-мемов", auth1, 2015);
-        book2.setPubYear(2016);
 
+        System.out.println("Автор первой книги: " + auth2);
         System.out.println("book1 = " + book1);
-        System.out.println("book2 = " + book2);
+        System.out.println("book2 (было)  = " + book2);
+        book2.setPubYear(2016);
+        System.out.println("book2 (стало) = " + book2);
 
     }
 }
